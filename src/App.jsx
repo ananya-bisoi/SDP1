d8d2d53b607d2d150964ce3d7ee2f099530ed5be
Ananya Bisoi <aanyabisoi996@gmail.com>
2026-01-05 15:59:54 +0530

    build vite react with tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+       <p className='bg-amber-300 text-amber-800 font-bold'>Hello I am Ananya Bisoi</p>
+    </>
+  )
+}
+
+export default App
